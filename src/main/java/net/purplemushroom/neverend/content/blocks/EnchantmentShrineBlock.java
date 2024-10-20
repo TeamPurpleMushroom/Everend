c8a63baaeb82f030956ea396afcf4746e4ae65b3
DiamondWalker <19913710+DiamondWalker@users.noreply.github.com>
2024-10-20 16:45:39 -0400

    Enchantment shrine functionality

--- src/main/java/net/purplemushroom/neverend/content/blocks/EnchantmentShrineBlock.java
+++ src/main/java/net/purplemushroom/neverend/content/blocks/EnchantmentShrineBlock.java
@@ -4,10 +4,14 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+
+import java.util.Map;
 
 public class EnchantmentShrineBlock extends Block {
     public EnchantmentShrineBlock(Properties p_49795_) {
@@ -15,7 +19,25 @@ public class EnchantmentShrineBlock extends Block {
     }
 
     @Override
-    public InteractionResult use(BlockState p_60503_, Level p_60504_, BlockPos p_60505_, Player p_60506_, InteractionHand p_60507_, BlockHitResult p_60508_) {
-        return super.use(p_60503_, p_60504_, p_60505_, p_60506_, p_60507_, p_60508_);
+    public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+        ItemStack item = player.getMainHandItem();
+        if (item.isEnchanted() && !(item.hasTag() && item.getTag().getBoolean("ShrineEnchanted"))) {
+            // TODO: consume crystal
+            if (!level.isClientSide()) {
+                boolean enchanted = false;
+                for (Map.Entry<Enchantment, Integer> enchantmentEntry : item.getAllEnchantments().entrySet()) {
+                    Enchantment enchant = enchantmentEntry.getKey();
+                    if (enchant.getMaxLevel() > 1) {
+                        item.enchant(enchant, enchantmentEntry.getValue() + 1); // FIXME: duplicate enchant
+                        enchanted = true;
+                    }
+                }
+                if (enchanted) {
+                    item.getOrCreateTag().putBoolean("ShrineEnchanted", true);
+                }
+            }
+            return InteractionResult.sidedSuccess(level.isClientSide);
+        }
+        return super.use(blockState, level, blockPos, player, hand, hitResult);
     }
 }
