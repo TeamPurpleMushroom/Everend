787ff1081ff926e1073dc38124d28f98d6ed610c
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2024-10-24 15:33:10 -0700

    Nullberry working better, but still bad

--- src/main/java/net/purplemushroom/neverend/content/items/NullberryItem.java
+++ src/main/java/net/purplemushroom/neverend/content/items/NullberryItem.java
@@ -1,8 +1,11 @@
 package net.purplemushroom.neverend.content.items;
 
+import com.google.common.collect.ImmutableList;
 import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerChunkCache;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.level.WorldGenRegion;
 import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
@@ -16,8 +19,12 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.LevelHeightAccessor;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.placement.HeightmapPlacement;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import net.purplemushroom.neverend.Neverend;
@@ -26,6 +33,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class NullberryItem extends Item {
     private final Logger LOGGER = Neverend.LOGGER;
@@ -36,10 +44,10 @@ public class NullberryItem extends Item {
     @Override
     public @NotNull ItemStack finishUsingItem(@NotNull ItemStack stack, @NotNull Level level, @NotNull LivingEntity player) {
         ItemStack heldItem = super.finishUsingItem(stack, level, player);
-        if (!level.isClientSide) {
+        if (!level.isClientSide()) {
             if (certainDoom(level, player)) {
                 LOGGER.info("Player passed requirements to be teleported");
-                handleTeleport((ServerLevel) level, (Player) player);
+                handleTeleport((ServerLevel) level, (ServerPlayer) player);
             }
         }
         return heldItem;
@@ -55,6 +63,7 @@ public class NullberryItem extends Item {
             LOGGER.info("Player damaged by void or falling below minimum Y level");
             return true;
         } else {
+            //TODO: when too close to the edge, thinks all blocks in AABB match air. Why?
             int distanceToVoid = Mth.clamp(playerY - voidY, voidY, level.getMaxBuildHeight());
             AABB belowBB = AABB.ofSize(player.position().subtract(0, distanceToVoid, 0), 1, distanceToVoid, 1);
             boolean allMatch = level.getBlockStatesIfLoaded(belowBB).allMatch(BlockBehaviour.BlockStateBase::isAir);
@@ -68,52 +77,48 @@ public class NullberryItem extends Item {
             if (allMatch) {
                 LOGGER.info("All match");
                 return true;
-            } else {
-                LOGGER.info("Void not detected");
             }
         }
         return false;
     }
 
     private void handleTeleport(ServerLevel serverLevel, Player serverPlayer) {
-        RandomSource random = serverPlayer.getRandom();
-        int maxDistance = 128;
-        int minHeight = serverLevel.getMinBuildHeight();
-        int maxHeight = serverLevel.getLogicalHeight() - 1;
         double playerX = serverPlayer.getX();
         double playerY = serverPlayer.getY();
         double playerZ = serverPlayer.getZ();
-        //for (int i = 0; i < 128; ++i) {
-            double randomX = playerX + (random.nextDouble() - 0.5) * maxDistance;
-            double randomY = Mth.clamp(playerY + random.nextInt(maxDistance) - 8, minHeight, maxHeight);
-            double randomZ = playerZ + (random.nextDouble() - 0.5) * maxDistance;
-            Entity mount = serverPlayer.getVehicle();
-            if (serverPlayer.isPassenger() && mount != null) {
-                List<Entity> passengers = new ArrayList<>(mount.getPassengers());
-                for (Entity entity : passengers) {
-                    if (entity instanceof LivingEntity passenger) {
-                        passenger.stopRiding();
-                        teleport(serverLevel, passenger, playerX, playerY, playerZ, randomX, randomY, randomZ);
-                        //break;
-                    }
+
+        //TODO: fix teleporting player in the air sometimes - might be caused by player crossing into chunk containing no solid blocks.
+        // Need to find a way to scan for the nearest chunk containing a solid block
+        int chunkX = serverPlayer.chunkPosition().getMinBlockX();
+        int chunkZ = serverPlayer.chunkPosition().getMinBlockZ();
+        BlockPos heightmapPos = serverLevel.getHeightmapPos(Heightmap.Types.MOTION_BLOCKING, serverLevel.getBlockRandomPos(chunkX, 0, chunkZ, 32));
+        double randomX = heightmapPos.getX();
+        double randomY = heightmapPos.getY();
+        double randomZ = heightmapPos.getZ();
+
+        Entity mount = serverPlayer.getVehicle();
+        if (serverPlayer.isPassenger() && mount != null) {
+            List<Entity> passengers = new ArrayList<>(mount.getPassengers());
+            for (Entity entity : passengers) {
+                if (entity instanceof LivingEntity passenger) {
+                    passenger.stopRiding();
+                    teleport(serverLevel, passenger, playerX, playerY, playerZ, randomX, randomY, randomZ);
                 }
-                if (mount instanceof LivingEntity livingEntity) {
-                    teleport(serverLevel, livingEntity, playerX, playerY, playerZ, randomX, randomY, randomZ);
-                    //break;
-                }
-            } else {
-                teleport(serverLevel, serverPlayer, playerX, playerY, playerZ, randomX, randomY, randomZ);
-                //break;
             }
-        //}
+            if (mount instanceof LivingEntity livingEntity) {
+                teleport(serverLevel, livingEntity, playerX, playerY, playerZ, randomX, randomY, randomZ);
+            }
+        } else {
+            teleport(serverLevel, serverPlayer, playerX, playerY, playerZ, randomX, randomY, randomZ);
+        }
+        serverPlayer.getCooldowns().addCooldown(this, 20);
     }
 
     private void teleport(ServerLevel serverLevel, LivingEntity entity, double x, double y, double z, double x1, double y1, double z1) {
-        if (entity.randomTeleport(x1, y1, z1, true)) {
-            serverLevel.gameEvent(GameEvent.TELEPORT, entity.position(), GameEvent.Context.of(entity));
-            SoundEvent soundevent = SoundEvents.CHORUS_FRUIT_TELEPORT;
-            serverLevel.playSound(null, x, y, z, soundevent, SoundSource.PLAYERS, 1.0F, 1.0F);
-            entity.playSound(soundevent, 1.0F, 1.0F);
-        }
+        entity.teleportTo(x1, y1, z1);
+        serverLevel.gameEvent(GameEvent.TELEPORT, entity.position(), GameEvent.Context.of(entity));
+        SoundEvent soundevent = SoundEvents.CHORUS_FRUIT_TELEPORT;
+        serverLevel.playSound(null, x, y, z, soundevent, SoundSource.PLAYERS, 1.0F, 1.0F);
+        entity.playSound(soundevent, 1.0F, 1.0F);
     }
 }
