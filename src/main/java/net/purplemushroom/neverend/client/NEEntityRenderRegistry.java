4a5f18a3a184c33745eb68d45dc74ce14f7dffa3
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2024-10-30 18:28:02 -0700

    Gordon renderer

---
+++ src/main/java/net/purplemushroom/neverend/client/NEEntityRenderRegistry.java
@@ -0,0 +1,21 @@
+package net.purplemushroom.neverend.client;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.purplemushroom.neverend.Neverend;
+import net.purplemushroom.neverend.client.render.Entity2DRenderer;
+import net.purplemushroom.neverend.registry.NEEntities;
+
+@Mod.EventBusSubscriber(modid = Neverend.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class NEEntityRenderRegistry {
+
+    @SubscribeEvent
+    public static void registerEntityRenders(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(NEEntities.GORDON_TYPE.get(), manager -> new Entity2DRenderer<>(manager, Neverend.tl("entity/gordon"))
+                .fullbright(true)
+                .projectile(true)
+                .scale(5.5F));
+    }
+}
--- src/main/java/net/purplemushroom/neverend/content/entities/Gordon.java
+++ src/main/java/net/purplemushroom/neverend/content/entities/Gordon.java
@@ -3,6 +3,9 @@ package net.purplemushroom.neverend.content.entities;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.level.Level;
 
 public class Gordon extends Entity {
@@ -12,16 +15,20 @@ public class Gordon extends Entity {
 
     @Override
     protected void defineSynchedData() {
-
     }
 
     @Override
     protected void readAdditionalSaveData(CompoundTag compoundTag) {
-
     }
 
     @Override
     protected void addAdditionalSaveData(CompoundTag compoundTag) {
+    }
 
+    public static AttributeSupplier.Builder createAttributes() {
+        return Mob.createMobAttributes()
+                .add(Attributes.MAX_HEALTH, 20.0D)
+                .add(Attributes.ATTACK_DAMAGE, 3.0D)
+                .add(Attributes.MOVEMENT_SPEED, 0.25D);
     }
 }
