c9edc6415ee903ee8fc7e82063c9c42c5e8466d6
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2024-10-30 17:57:56 -0700

    Gordon v2

--- src/main/java/net/purplemushroom/neverend/registry/NEEntities.java
+++ src/main/java/net/purplemushroom/neverend/registry/NEEntities.java
@@ -6,6 +6,7 @@ import net.purplemushroom.neverend.Neverend;
 import net.purplemushroom.neverend.content.entities.Gordon;
 import ru.timeconqueror.timecore.api.registry.EntityRegister;
 import ru.timeconqueror.timecore.api.registry.util.AutoRegistrable;
+import ru.timeconqueror.timecore.api.registry.util.Promised;
 
 @AutoRegistrable.Entries("entity")
 public class NEEntities {
@@ -13,7 +14,7 @@ public class NEEntities {
     @AutoRegistrable
     private static final EntityRegister ENTITIES = new EntityRegister(Neverend.MODID);
 
-    public static final EntityType<Gordon> GORDON_TYPE = ENTITIES.register("gordon", EntityType.Builder.of(Gordon::new, MobCategory.UNDERGROUND_WATER_CREATURE)).asPromised().get();
+    public static final Promised<EntityType<Gordon>> GORDON_TYPE = ENTITIES.register("gordon", EntityType.Builder.of(Gordon::new, MobCategory.UNDERGROUND_WATER_CREATURE)).asPromised();
 }
 
 
