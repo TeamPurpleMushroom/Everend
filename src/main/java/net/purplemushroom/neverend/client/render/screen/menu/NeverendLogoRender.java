8eb06bc40eabc1bbbefddc68bb85851939bf34db
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2024-11-07 17:45:32 -0700

    NE Menu logo

--- src/main/java/net/purplemushroom/neverend/client/render/screen/menu/NeverendLogoRender.java
+++ src/main/java/net/purplemushroom/neverend/client/render/screen/menu/NeverendLogoRender.java
@@ -6,7 +6,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.purplemushroom.neverend.Neverend;
 
 class NeverendLogoRender extends LogoRenderer {
-    public static final ResourceLocation NEVEREND_LOGO = Neverend.rl("textures/logo.png");
+    public static final ResourceLocation NEVEREND_LOGO = Neverend.rl("textures/gui/logo.png");
 
     public NeverendLogoRender() {
         super(false);
@@ -16,7 +16,7 @@ class NeverendLogoRender extends LogoRenderer {
     public void renderLogo(GuiGraphics pGuiGraphics, int pScreenWidth, float pTransparency, int pHeight) {
         pGuiGraphics.setColor(1.0F, 1.0F, 1.0F, pTransparency);
         int i = pScreenWidth / 2 - 128;
-        pGuiGraphics.blit(NEVEREND_LOGO, i, pHeight, 0.0F, 0.0F, 256, 44, 256, 64);
+        pGuiGraphics.blit(NEVEREND_LOGO, i, pHeight, 0.0F, 0.0F, 256, 47, 256, 47);
         pGuiGraphics.setColor(1.0F, 1.0F, 1.0F, 1.0F);
     }
 }
