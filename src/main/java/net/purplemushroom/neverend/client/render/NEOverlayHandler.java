67099c0e9a88d341d8f041efde82bfce104258f6
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2024-11-07 18:06:09 -0700

    I tried

---
+++ src/main/java/net/purplemushroom/neverend/client/render/NEOverlayHandler.java
@@ -0,0 +1,19 @@
+package net.purplemushroom.neverend.client.render;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.purplemushroom.neverend.Neverend;
+import net.purplemushroom.neverend.client.render.overlay.RiftFishingOverlay;
+
+@Mod.EventBusSubscriber(modid= Neverend.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class NEOverlayHandler {
+    @SubscribeEvent
+    public static void renderGUI(RegisterGuiOverlaysEvent event) {
+        event.registerAboveAll("rift_fishing_progress", (gui, guiGraphics, partialTick, screenWidth, screenHeight) -> {
+            gui.setupOverlayRenderState(true, false);
+            RiftFishingOverlay.render(gui, guiGraphics, partialTick, screenWidth, screenHeight);
+        });
+    }
+}
