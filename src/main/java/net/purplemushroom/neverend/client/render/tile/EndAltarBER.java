956984f28bf19e0297f13b91f4c53bdd09c96efb
DiamondWalker <19913710+DiamondWalker@users.noreply.github.com>
2025-02-26 18:18:06 -0500

    End altar item rendering finished

--- src/main/java/net/purplemushroom/neverend/client/render/tile/EndAltarBER.java
+++ src/main/java/net/purplemushroom/neverend/client/render/tile/EndAltarBER.java
@@ -1,12 +1,16 @@
 package net.purplemushroom.neverend.client.render.tile;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.math.Axis;
+import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.Direction;
+import net.minecraft.util.Mth;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -26,12 +30,16 @@ public class EndAltarBER implements BlockEntityRenderer<EndAltarBlockEntity> {
         //BakedModel model = itemRenderer.getModel(item, blockEntity.getLevel(), null, 0);
         if (level != null && !item.isEmpty()) {
 
+            float time = partialTick + level.getGameTime();
             poseStack.pushPose();
-            poseStack.translate(0.0f, 1.0f, 0.0f);
+            poseStack.translate(0.5f, 1.6f, 0.5f);
+            poseStack.translate(0.0f, 0.1f * Mth.sin(time / 10), 0.0f);
+            poseStack.scale(0.5f, 0.5f, 0.5f);
+            poseStack.mulPose(Axis.YP.rotationDegrees(time));
             this.itemRenderer.renderStatic(
                     item,
                     ItemDisplayContext.FIXED,
-                    packedLight,
+                    LevelRenderer.getLightColor(level, blockEntity.getBlockState(), blockEntity.getBlockPos().relative(Direction.UP)),//packedLight,
                     OverlayTexture.NO_OVERLAY,
                     poseStack, buffer,
                     blockEntity.getLevel(),
