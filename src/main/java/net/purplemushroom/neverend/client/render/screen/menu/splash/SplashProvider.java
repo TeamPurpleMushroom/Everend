e129cea4e8a1170a98223427878e197c511929b9
DiamondWalker <19913710+DiamondWalker@users.noreply.github.com>
2025-02-28 20:18:48 -0500

    Update SplashProvider.java

--- src/main/java/net/purplemushroom/neverend/client/render/screen/menu/splash/SplashProvider.java
+++ src/main/java/net/purplemushroom/neverend/client/render/screen/menu/splash/SplashProvider.java
@@ -172,7 +172,9 @@ public class SplashProvider {
             "Wahoo!",
             "1-Up!",
             "Tower of pimps!",
-            "Let's play!"
+            "Let's play!",
+            "A series of unfortunate events!",
+            "A neverending story!"
     };
 
     public static final String[] SPOOKY_SPLASHES = {
