34bf3b7a713bd85d19032f601e2e9de8ca3986d8
DiamondWalker <19913710+DiamondWalker@users.noreply.github.com>
2025-03-14 09:48:58 -0400

    Maybe fix idk

--- src/main/java/net/purplemushroom/everend/content/effect/RiftTorn.java
+++ src/main/java/net/purplemushroom/everend/content/effect/RiftTorn.java
@@ -2,6 +2,8 @@ package net.purplemushroom.everend.content.effect;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.purplemushroom.everend.util.BitUtil;
@@ -12,5 +14,13 @@ public class RiftTorn extends MobEffect {
     public RiftTorn() {
         super(MobEffectCategory.HARMFUL, BitUtil.rgbToInt(7, 105, 92));
         addAttributeModifier(Attributes.MAX_HEALTH, "0BFDF211-4CFF-400D-AEE3-C94E4C4ECF80", -2.0f, AttributeModifier.Operation.ADDITION);
+    }
+
+    @Override
+    public void addAttributeModifiers(LivingEntity pLivingEntity, AttributeMap pAttributeMap, int pAmplifier) {
+        super.addAttributeModifiers(pLivingEntity, pAttributeMap, pAmplifier);
+        if (pLivingEntity.getHealth() > pLivingEntity.getMaxHealth()) {
+            pLivingEntity.setHealth(pLivingEntity.getMaxHealth());
+        }
     }
 }
