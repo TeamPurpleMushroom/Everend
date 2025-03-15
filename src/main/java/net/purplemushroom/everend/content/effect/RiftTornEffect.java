98bd518fb9a4180c3bbf385c1af385d48c196cba
DiamondWalker <19913710+DiamondWalker@users.noreply.github.com>
2025-03-15 12:54:38 -0400

    Rift torn effect can no longer be cured

--- src/main/java/net/purplemushroom/everend/content/effect/RiftTornEffect.java
+++ src/main/java/net/purplemushroom/everend/content/effect/RiftTornEffect.java
@@ -7,12 +7,21 @@ import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeMap;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.item.ItemStack;
 import net.purplemushroom.everend.util.BitUtil;
+
+import java.util.Collections;
+import java.util.List;
 
 public class RiftTornEffect extends MobEffect {
     public RiftTornEffect() {
         super(MobEffectCategory.HARMFUL, BitUtil.rgbToInt(7, 105, 92));
         addAttributeModifier(Attributes.MAX_HEALTH, "0BFDF211-4CFF-400D-AEE3-C94E4C4ECF80", -2.0f, AttributeModifier.Operation.ADDITION);
+    }
+
+    @Override
+    public List<ItemStack> getCurativeItems() {
+        return Collections.emptyList();
     }
 
     @Override
